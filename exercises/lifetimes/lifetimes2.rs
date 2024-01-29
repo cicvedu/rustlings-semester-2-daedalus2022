1b78bc89d6fd0d3733193ed07b79113d3ef29b3b
r730 <r730@163.com>
2024-01-29 15:25:09 +0800

    fixed lifetimes

--- exercises/lifetimes/lifetimes2.rs
+++ exercises/lifetimes/lifetimes2.rs
@@ -19,9 +19,7 @@ fn longest<'a>(x: &'a str, y: &'a str) -> &'a str {
 fn main() {
     let string1 = String::from("long string is long");
     let result;
-    {
-        let string2 = String::from("xyz");
-        result = longest(string1.as_str(), string2.as_str());
-    }
+    let string2 = String::from("xyz");
+    result = longest(string1.as_str(), string2.as_str());
     println!("The longest string is '{}'", result);
 }
